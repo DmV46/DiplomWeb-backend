110590a1f53bf53d5af8a93f04e8d7189c188722
Dmitry V <wel4692@gmail.com>
2020-05-01 13:41:06 +0300

    add corsOptions

--- configuration/settings.js
+++ configuration/settings.js
@@ -4,9 +4,14 @@ const { DEV_SECRET } = require('./constants');
 const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/diplomweb';
 const isProduction = process.env.NODE_ENV === 'production';
+const corsOptions = {
+  origin: '*',
+  method: 'GET, POST OPTIONS, DELETE, HEAD',
+};
 
 module.exports = {
   PORT,
   DATABASE_URL,
   JWT_SECRET: isProduction ? process.env.JWT_SECRET : DEV_SECRET,
+  corsOptions,
 };
