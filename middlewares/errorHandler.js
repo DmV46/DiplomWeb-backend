3bb228463ddf09c5ac9a8875c719d1e6569dd2fd
Dmitry V <wel4692@gmail.com>
2020-05-01 14:20:07 +0300

    delete cors module

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,7 @@
-const { isCelebrate } = require('celebrate');
-
 const { SERVER_ERROR } = require('../configuration/constants');
 
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  if (isCelebrate(err)) {
-    return res.status(400).send({ message: err.joi.message });
-  }
   return res.status(statusCode).send({ message: statusCode === 500 ? SERVER_ERROR : message });
 };
 
